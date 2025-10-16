acd098f2330435ef817051f34a3a9f134bcf2f4d
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2025-10-16 20:20:17 +0200

    Create _layout.jsx

---
+++ app/_layout.jsx
@@ -0,0 +1,41 @@
+import { StatusBar } from "expo-status-bar";
+import { Drawer } from "expo-router/drawer";
+
+export default function Layout() {
+  return (
+    <Drawer>
+      <Drawer.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerStyle: { backgroundColor: "#FF7F50" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerTitleAlign: "center",
+        }}
+      />
+      <Drawer.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          headerStyle: { backgroundColor: "#FF7F50" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerTitleAlign: "center",
+        }}
+      />
+      <Drawer.Screen
+        name="about"
+        options={{
+          title: "About",
+          headerStyle: { backgroundColor: "#FF7F50" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+          headerTitleAlign: "center",
+        }}
+      />
+
+      <StatusBar style="light" />
+    </Drawer>
+  );
+}
