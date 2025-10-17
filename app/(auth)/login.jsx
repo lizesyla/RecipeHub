4eba69b20c7518934fde1ed46ee86bce04d92b49
Rozeta <rozeta.meshi@student.uni-pr.edu>
2025-10-17 22:18:07 +0200

    added login

---
+++ app/(auth)/login.jsx
@@ -0,0 +1,49 @@
+import { Pressable, StyleSheet , Text} from 'react-native'
+import {Link} from 'expo-router'
+import { Colors } from '../../constants/Colors'
+
+import ThemedView from '../../components/ThemedView'
+import ThemedText from '../../components/ThemedText'
+import Spacer from '../../components/Spacer'
+
+const Login = () => {
+  return (
+   <ThemedView style={styles.container}>
+
+    <Spacer />
+    <ThemedText title={true} style={styles.title}>
+        Login to your account
+    </ThemedText>
+
+    <Pressable style={({pressed}) => [styles.btn, pressed && styles.pressed ]}>
+        <Text style={{color: '#f2f2f2' }}>Login</Text>
+
+    </Pressable>
+     
+
+   </ThemedView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 18,
+        marginBottom: 30
+    },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+      },
+      pressed: {
+        opacity: 0.8,
+      },
+      
+})
