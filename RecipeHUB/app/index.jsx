f58d6595a6c5f9f317a5cc8ecbd6d3119f441dd5
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2025-10-18 18:02:14 +0200

    login screen style

--- RecipeHUB/app/index.jsx
+++ RecipeHUB/app/index.jsx
@@ -6,20 +6,23 @@ export default function Login() {
   const router = useRouter();
 
   return (
-    <View>
-      <Text>Log In</Text>
+    <View  style={styles.container}>
+      <Text style={styles.title}>Log In</Text>
 
       <TextInput
+        style={styles.input}
         placeholder="Email"
         placeholderTextColor="#aaa"
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry
         placeholderTextColor="#aaa"
       />
 
       <TouchableOpacity
+        style={styles.arrowButton}
         onPress={() => router.push("/(tabs)/home")}
       >
         <Ionicons name="arrow-forward-circle" size={60} color="#4CAF50" />
@@ -27,5 +30,25 @@ export default function Login() {
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#111",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    title: { fontSize: 26, fontWeight: "bold", color: "#fff", marginBottom: 40 },
+    input: {
+      width: "80%",
+      backgroundColor: "#222",
+      color: "#fff",
+      padding: 12,
+      borderRadius: 10,
+      marginVertical: 8,
+    },
+    arrowButton: { marginTop: 30 },
+  });
 
 
