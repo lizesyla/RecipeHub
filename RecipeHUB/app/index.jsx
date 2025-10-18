da073fc86b0f464659f6d064d5d2d95f996b0d48
PranveraGashi <121582082+Pranvera21@users.noreply.github.com>
2025-10-19 00:33:12 +0200

    signUp button added

--- RecipeHUB/app/index.jsx
+++ RecipeHUB/app/index.jsx
@@ -6,7 +6,7 @@ export default function Login() {
   const router = useRouter();
 
   return (
-    <View  style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Log In</Text>
 
       <TextInput
@@ -27,28 +27,52 @@ export default function Login() {
       >
         <Ionicons name="arrow-forward-circle" size={60} color="#4CAF50" />
       </TouchableOpacity>
+
+      <View style={styles.signUpView}>
+        <Text style={styles.accountText}>Don't have an account?</Text>
+        <TouchableOpacity
+          onPress={() => router.push("/register")}
+        >
+          <Text style={styles.linkText}> Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#111",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    title: { fontSize: 26, fontWeight: "bold", color: "#fff", marginBottom: 40 },
-    input: {
-      width: "80%",
-      backgroundColor: "#222",
-      color: "#fff",
-      padding: 12,
-      borderRadius: 10,
-      marginVertical: 8,
-    },
-    arrowButton: { marginTop: 30 },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#111",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: { fontSize: 26, fontWeight: "bold", color: "#fff", marginBottom: 40 },
+  input: {
+    width: "80%",
+    backgroundColor: "#222",
+    color: "#fff",
+    padding: 12,
+    borderRadius: 10,
+    marginVertical: 8,
+  },
+  arrowButton: { marginTop: 30 },
+  linkText: {
+    color: "#4CAF50",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  accountText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  signUpView: {
+    flexDirection: "row",
+    marginTop: 20,
+  }
+});
 
 
