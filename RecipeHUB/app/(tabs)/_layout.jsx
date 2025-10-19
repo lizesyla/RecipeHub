4784fe32792994d9a08f1b1999f52c5145e3e1a4
Erisa Sollova <erisasollova@gmail.com>
2025-10-19 22:33:49 +0200

    Update tabs

---
+++ RecipeHUB/app/(tabs)/_layout.jsx
@@ -0,0 +1,46 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: "#4CAF50" },
+        headerTintColor: "#fff",
+        tabBarStyle: { backgroundColor: "#222", height: 65, paddingBottom: 5 },
+        tabBarActiveTintColor: "#4CAF50",
+        tabBarInactiveTintColor: "#aaa",
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => <Ionicons name="home-outline" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => <Ionicons name="search-outline" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => <Ionicons name="add-circle-outline" color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" color={color} size={size} />,
+          headerShown: false, // <-- hiq header-in nga TabsLayout, merr header-in e vet nga Drawer-i
+        }}
+      />
+    </Tabs>
+  );
+}
